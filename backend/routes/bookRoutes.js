335dc15c9f5e8dc2c303da56ef67842b8f5d8f33
Omoreno <jxhavi@gmail.com>
2025-07-31 14:42:21 -0500

    se agregaron algunos comentarios

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -19,7 +19,7 @@ router.get('/:id', getBookById);
 // Rutas protegidas (necesitan token)
 router.post('/', protect, createBook); // Solo usuarios autenticados pueden crear
 router.put('/:id', protect, updateBook); // Solo usuarios autenticados pueden actualizar
-router.delete('/:id', protect, deleteBook);
-router.post('/import', protect, createBulkBooks); // Solo usuarios autenticados pueden eliminar
+router.delete('/:id', protect, deleteBook);// solo usuarios autenticados pueden eliminar
+router.post('/import', protect, createBulkBooks); // Solo usuarios autenticados pueden cargar libros en masa
 
 module.exports = router;
